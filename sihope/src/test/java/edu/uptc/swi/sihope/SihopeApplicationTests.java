8f066f0c09d84a9f5ba852dc1df09ed5bb5a98b8
CamilaPuin <diana.puin01@uptc.edu.co>
2026-07-06 14:41:30 -0500

    Main Creación del proyecto

---
+++ sihope/src/test/java/edu/uptc/swi/sihope/SihopeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uptc.swi.sihope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SihopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
